a07fb28715ecde4427863a99b1a1d66313124ea0
Sarmad <afzalsarmad77@gmail.com>
2022-10-27 16:49:56 -0400

    added token retrieval + expiration time for token

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //! get list of users
 
@@ -9,7 +10,7 @@ router.get("/", async (req, res) => {
   const userList = await User.find().select("-passwordHash");
   /* If you'd like to only get certain fields, you can simply pass them 
      as parameters to the select function
-     * User.find().select("name phone email"); 
+     ? User.find().select("name phone email"); 
      this returns just the name, phone, and email of users 
   */
 
@@ -44,16 +45,26 @@ router.post("/", async (req, res) => {
   res.send(user);
 });
 
-// * making sure that user is in the system based on their identifying element (this case the email)
+// ! making sure that user is in the system based on their identifying element (this case the email)
+
 router.post("/login", async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
+  const secret = process.env.secret;
 
   if (!user) {
     return res.status(400).send("This user has not been found");
   }
 
   if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-    res.status(200).send("User Authenticated");
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      secret,
+      { expiresIn: "1d" } // can change to 1w, 1y.. etc
+    );
+
+    res.status(200).send({ user: user.email, token: token });
   } else {
     res.status(400).send("Wrong Password");
   }
